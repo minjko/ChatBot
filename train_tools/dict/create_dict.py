d014bf1dbc2dd270522705b752348b1f96962218
minjko <komj36@gmail.com>
2023-02-27 17:07:51 +0900

    (2) construct dict & tensorflow.keras importing error

--- train_tools/dict/create_dict.py
+++ train_tools/dict/create_dict.py
@@ -2,7 +2,7 @@
 # 챗봇에서 사용하는 사전 파일 생성
 #
 from utils.Preprocess import Preprocess
-from tensorflow.keras import preprocessing
+from keras import preprocessing # 원래 from tensorflow.keras import preprocessing이나, 라이브러리 import 문제로 tensorflow.keras가 아닌 keras만 사용함. preprocessing만 사용할 것이므로 괜찮지 않을까 생각 중
 import pickle
 
 # 말뭉치 데이터 읽어오기
